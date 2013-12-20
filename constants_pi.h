0e14189e99f4775cbad58235d99aa9d7527a9035
orange224 <orange224@gmail.com>
2013-12-20 11:29:11 +0800

    Create constants_pi.h

---
+++ constants_pi.h
@@ -0,0 +1,8 @@
+      
+      real(kind=8), parameter :: qpi =  0.7853981633974483096156608458199d0   ! pi/4
+      real(kind=8), parameter :: hpi =  1.5707963267948966192313216916398d0   ! pi/2
+      
+      real(kind=8), parameter ::  pi =  3.1415926535897932384626433832795d0   ! pi
+      
+      real(kind=8), parameter :: epi =  6.2831853071795864769252867665590d0   ! pi*2
+      real(kind=8), parameter :: fpi = 12.5663706143591729538505735331180d0   ! pi*4
