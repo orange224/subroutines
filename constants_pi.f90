d92934a09e484cf3d61996e0fcbb101d6074f0c2
orange224 <orange224@gmail.com>
2014-01-27 13:58:44 +0800

    Update constants_pi.f90

--- constants_pi.f90
+++ constants_pi.f90
@@ -1,9 +1,12 @@
       
-      real(kind=8), parameter :: qpi =  0.7853981633974483096156608458199d0   ! pi/4
-      real(kind=8), parameter :: hpi =  1.5707963267948966192313216916398d0   ! pi/2
       
-      real(kind=8), parameter ::  pi =  3.1415926535897932384626433832795d0   ! pi
+      real(kind=8), parameter :: qpi =  0.7853981633974483096156608458199d0   ! N[pi/4, 32]
+      real(kind=8), parameter :: hpi =  1.5707963267948966192313216916398d0   ! N[pi/2, 32]
       
-      real(kind=8), parameter :: epi =  6.2831853071795864769252867665590d0   ! pi*2
-      real(kind=8), parameter :: fpi = 12.5663706143591729538505735331180d0   ! pi*4
-
+      real(kind=8), parameter ::  pi =  3.1415926535897932384626433832795d0   ! N[  pi, 32]
+      
+      real(kind=8), parameter :: epi =  6.2831853071795864769252867665590d0   ! N[2*pi, 32]
+      real(kind=8), parameter :: fpi = 12.5663706143591729538505735331180d0   ! N[4*pi, 32]
+      
+      
+      
