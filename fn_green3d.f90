800f079993cf856e4a5a2c0ea6cd7a6ef3a44529
orange224 <orange224@gmail.com>
2014-06-10 00:40:27 +0800

    Update fn_green3d.f90

--- fn_green3d.f90
+++ fn_green3d.f90
@@ -3,28 +3,33 @@
       !!  Chapter 3. Green's Functions for the Wave Equation
       !!  equation (3.5.11)
       
-      function green3d (vp, sx, sy, sz, rx, ry, rz, tt, dt, t0) result (dg)
-      
-      implicit none
-         real(kind=8), intent(in) :: vp
-         real(kind=8), intent(in) :: sx, sy, sz
-         real(kind=8), intent(in) :: rx, ry, rz
-         real(kind=8), intent(in) :: tt, dt, t0
+      FUNCTION green3d (vp, sx, sy, sz, gx, gy, gz, tt, t0, dt) RESULT (gg)
+      IMPLICIT NONE
+         REAL(KIND=8), INTENT(IN) :: vp
+         REAL(KIND=8), INTENT(IN) :: sx, sy, sz
+         REAL(KIND=8), INTENT(IN) :: gx, gy, gz
+         REAL(KIND=8), INTENT(IN) :: tt, t0, dt
+         OPTIONAL                 :: dt
          
-         include 'constants_pi.f90'
-         real(kind=8) :: rr, dg
+         INCLUDE 'uwa_pi.f90'
+         REAL(KIND=8) :: rr, r0, r1, gg
          
-         rr = dsqrt((rx-sx)**2 + (ry-sy)**2 + (rz-sz)**2)
-         dg = 0.d0
+         rr = DSQRT((gx-sx)**2 + (gy-sy)**2)
+         r0 = DSQRT((gz-sz)**2 + rr**2)      ! real source
          
-         if (tt-t0 .eq. rr/vp) then
-            dg = 1.d0/(fpi*rr)
-            return
-            
-         else if (tt-t0-0.99d0*dt .le. rr/vp .and. tt-t0+0.99d0*dt .gt. rr/vp) then
-            dg = 1.d0/(fpi*rr)
-            return
-         end if
+         gg = 0.0D+0
+         IF (vp*(tt-t0) .LT. r0) gg = 0.0D+0
+         IF (vp*(tt-t0) .GT. r0) gg = 0.0D+0
+         IF (vp*(tt-t0) .EQ. r0) gg = 1.0D+0
          
-      end function green3d
+         IF (PRESENT(dt)) THEN
+            gg = 0.0D+0
+            IF (r0/vp .GT. tt-t0) gg = 0.0D+0
+            IF (r0/vp .LT. tt-t0) gg = 0.0D+0
+            IF (r0/vp .GT. tt-t0-0.5D+0*dt .AND. r0/vp .LE. tt-t0+0.5D+0*dt) gg = 1.0D+0
+         END IF
+         gg = gg*(1.0D+0/(fpi*r0))
+         
+         RETURN
+      END FUNCTION green3d
       
